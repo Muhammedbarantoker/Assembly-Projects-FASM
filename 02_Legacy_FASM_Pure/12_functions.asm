f738b0d3c107feeed93436c53f11a8c1bf118a12
Muhammed Baran Toker <muhammedbarantoker@gmail.com>
2026-05-01 18:04:59 +0300

    Refactor: Modular assembly structure with atoi and itoa functions

--- 02_Legacy_FASM_Pure/12_functions.asm
+++ 02_Legacy_FASM_Pure/12_functions.asm
@@ -7,25 +7,25 @@ entry $
 
     mov rax, 0
     mov rdi, 0
-    mov rsi, girdi_tamponu
+    mov rsi, input_buffer
     mov rdx, 20
     syscall
 
 
-    mov rsi, girdi_tamponu
+    mov rsi, input_buffer
     call atoi
 
 
     add rax, 10
 
 
-    mov rdi, sonuc_tamponu + 19
+    mov rdi, buffer + 19
     call itoa
 
 
     mov rax, 1
     mov rdi, 1
-    mov rsi, sonuc_tamponu
+    mov rsi, buffer
     mov rdx, 21
     syscall
 
@@ -37,32 +37,32 @@ entry $
 
 atoi:
     xor rax, rax
-.dongu:
+.parse_loop:
     movzx rdx, byte [rsi]
     cmp dl, 10
-    je .bitti
+    je .done
     sub dl, 48
     imul rax, 10
     add rax, rdx
     inc rsi
-    jmp .dongu
-.bitti:
+    jmp .parse_loop
+.done:
     ret
 
 
 itoa:
     mov rbx, 10
-.dongu:
+.convert_loop:
     xor rdx, rdx
     div rbx
     add dl, 48
     mov [rdi], dl
     dec rdi
     test rax, rax
-    jnz .dongu
+    jnz .convert_loop
     ret
 
 
 segment readable writeable
-    girdi_tamponu db 20 dup(0)
-    sonuc_tamponu db 20 dup(32), 10
+    input_buffer db 20 dup(0)
+    buffer db 20 dup(32), 10
