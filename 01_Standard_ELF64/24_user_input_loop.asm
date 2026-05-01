0b5a2a76a640347e04c2ca383743bd881a7a2902
Muhammed Baran Toker <muhammedbarantoker@gmail.com>
2026-05-02 02:50:04 +0300

    feat(24_user_input_loop): implement user-defined loop with two-digit formatting

---
+++ 01_Standard_ELF64/24_user_input_loop.asm
@@ -0,0 +1,73 @@
+format ELF64 executable 3
+
+
+segment readable executable
+entry _start
+
+
+_start:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input_buf
+    mov rdx, 2
+    syscall
+
+
+    mov al, byte [input_buf]
+    sub al, 48
+    movzx r14, al
+
+
+    mov r13, 1
+
+
+check_loop:
+    cmp r13, r14
+    jg end_program
+
+
+    mov rax, r13
+    call print_two_digits
+
+
+    inc r13
+    jmp check_loop
+
+
+end_program:
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+
+print_two_digits:
+    mov bl, 10
+    div bl
+
+
+    add al, 48
+    add ah, 48
+
+
+    mov byte [output_buf], al
+    mov byte [output_buf + 1], ah
+    mov byte [output_buf + 2], 10
+
+
+    mov r12, rax
+
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, output_buf
+    mov rdx, 3
+    syscall
+
+
+    mov rax, r12
+    ret
+
+
+segment readable writeable
+    input_buf rb 2
+    output_buf rb 3
